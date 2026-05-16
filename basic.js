30ff7ad9a11c640613871ca087dfd96f42a5b7bf
Your Name <akashmondkar03@gmail.com>
2026-05-16 20:16:19 +0530

    basic and expot and required

---
+++ basic.js
@@ -0,0 +1,24 @@
+ let a =100
+ var b= 3000;
+
+ function fruit(item){
+    console.log( " this is a fruit " + item)
+ }
+
+ fruit("banana")
+
+  for ( let a1 = 0 ;a1 <=10;a1++){
+    console.log(a1);
+  }
+ 
+  var user={
+    name:'akash',
+    city:'mumbai',
+    age:'29'
+  }
+
+  console.log(user);
+
+  const {userName} = require('./data')
+
+  console.log(userName);
