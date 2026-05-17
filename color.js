edc963b8eebb67b38f55e27d193dad309947004d
Your Name <akashmondkar03@gmail.com>
2026-05-17 10:21:33 +0530

    server

--- color.js
+++ color.js
@@ -4,4 +4,5 @@ console.log(colors.red("Hi this is akash"));
 console.log(colors.yellow("Hi this is akash"));
 console.log(colors.green("Hi this is akash"));
 console.log(colors.grey("Hi this is akash"));
-console.log(colors.bgBlue("Hi this is akash"));
+
+console.log(colors.blue("Hi this is akash"));
