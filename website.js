2189d87848a11f458ff57018557a226ca350e638
Your Name <akashmondkar03@gmail.com>
2026-05-18 23:09:45 +0530

    updated css

--- website.js
+++ website.js
@@ -1,8 +1,35 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
-const server =http.createServer((req,resp)=>{
-    resp.write("page check")
-    resp.end();
+const server = http.createServer((req, resp) => {
+    
+    if(req.url=='/'){
+ 
+        
+    fs.readFile("html/web.html", "utf-8", (error, data) => {
+      if (error) {
+        resp.writeHead(500, { "Content-type": "text/plain" });
+        resp.end("internal server error");
+        return false;
+      }
+        resp.write(data);
+        resp.end();
+      
+    });
+    }else if(req.url=='/style.css'){
+        
+    fs.readFile("html/style.css", "utf-8", (error, data) => {
+      if (error) {
+        resp.writeHead(500, { "Content-type": "text/plain" });
+        resp.end("css not found");
+        return false;
+      }
+        resp.writeHead(200, { "Content-type": "text/css" });
+        
+        resp.end(data);
+      
+    });
+    }
 
 
-}).listen("3003")
+  }).listen("3003");
