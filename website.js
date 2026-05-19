81fd3236ed02f0416da9f61f3ca519c6540a49c7
Your Name <akashmondkar03@gmail.com>
2026-05-19 22:34:38 +0530

    fix error

--- website.js
+++ website.js
@@ -2,63 +2,65 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, resp) => {
-    
-//===================================
-let collectHeaderData;
-fs.readFile("html/header.html", "utf-8", (error, headerData) => {
-      if (error) {
-        resp.writeHead(500, { "Content-type": "text/plain" });
-        resp.end("internal server error");
-        return false;
-      }
 
-      collectHeaderData=headerData
-        // resp.write(headerData);
-        // resp.end();
-      
-    });
+    // Reusable Header
+    let collectHeaderData = fs.readFileSync(
+        "html/header.html",
+        "utf-8"
+    );
 
+    // Home Route
+    if (req.url == '/') {
 
+        fs.readFile("html/web.html", "utf-8", (error, data) => {
 
+            if (error) {
 
+                resp.writeHead(500, {
+                    "Content-type": "text/plain"
+                });
 
+                resp.end("Internal Server Error");
 
-//=====================================
+                return;
+            }
 
+            resp.writeHead(200, {
+                "Content-type": "text/html"
+            });
 
+            resp.end(collectHeaderData + data);
 
+        });
 
-
-
-
-
-    if(req.url=='/'){
- 
-        
-    fs.readFile("html/web.html", "utf-8", (error, data) => {
-      if (error) {
-        resp.writeHead(500, { "Content-type": "text/plain" });
-        resp.end("internal server error");
-        return false;
-      }
-        resp.write(collectHeaderData+""+data);
-        resp.end();
-      
-    });
-    }else if(req.url=='/style.css'){
-        
-    fs.readFile("html/style.css", "utf-8", (error, data) => {
-      if (error) {
-        resp.writeHead(500, { "Content-type": "text/plain" });
-        resp.end("css not found");
-        return false;
-      }
-        resp.writeHead(200, { "Content-type": "text/css" });
-        
-        resp.end(data);
-      
-    });
     }
 
+    // CSS Route
+    else if (req.url == '/style.css') {
 
-  }).listen("3003");
+        fs.readFile("html/style.css", "utf-8", (error, data) => {
+
+            if (error) {
+
+                resp.writeHead(404, {
+                    "Content-type": "text/plain"
+                });
+
+                resp.end("CSS Not Found");
+
+                return;
+            }
+
+            resp.writeHead(200, {
+                "Content-type": "text/css"
+            });
+
+            resp.end(data);
+
+        });
+
+    }
+
+}).listen(3003);
+
+console.log("Server running on port 3003");
